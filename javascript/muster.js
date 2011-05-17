42eb742eb1b962088de49a12cd5fb6f4d56c58ff
Justin Force <justin.force@gmail.com>
2011-05-17 16:52:02 -0700

    Pretty up the example a little. Point to harold instead of localhost.

--- javascript/muster.js
+++ javascript/muster.js
@@ -1,12 +1,12 @@
 $(function() {
 
   var muster = $.muster({ 
-    url: "http://localhost:8080/muster/muster",
+    url: "http://harold.justinforce.com:8080/muster/muster",
       database: "courses",
   });
 
   var results = muster.query({
-    select: "*",
+    select: "course,Instructor_Name,description",
     from: "coursesyear",
     where: "\"year\" = '2010-11'",
     callback: function(results) {
