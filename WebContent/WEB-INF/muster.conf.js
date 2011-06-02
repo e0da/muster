d48a5f63f82ce9926a0ecf264faee4e8bce8203c
Justin Force <justin.force@gmail.com>
2011-06-02 16:43:11 -0700

    Fix DB connectivity resiliency; update config.

--- WebContent/WEB-INF/muster.conf.js
+++ WebContent/WEB-INF/muster.conf.js
@@ -3,14 +3,7 @@
     {
       "name" : "ggsedb",
       "url" : "jdbc:filemaker://newdb5.education.ucsb.edu/GGSEDB",
-      "username" : "jdbc",
-      "password" : "h4!ksa8%j37&",
-      "driver" : "com.filemaker.jdbc.Driver"
-    },
-    {
-      "name" : "courses",
-      "url" : "jdbc:filemaker://newdb5.education.ucsb.edu/courses",
-      "username" : "jdbc",
+      "username" : "muster",
       "password" : "h4!ksa8%j37&",
       "driver" : "com.filemaker.jdbc.Driver"
     }
